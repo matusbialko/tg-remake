1ad88cbe42403b0f0a4ef5d0b032d2fc1b138fba
Matus B <bialkomatus@gmail.com>
2024-08-27 16:02:26 +0200

    setup

---
+++ plugins/app/projects/updates/create_projects_table.php
@@ -0,0 +1,22 @@
+<?php namespace App\Projects\Updates;
+
+use Schema;
+use October\Rain\Database\Schema\Blueprint;
+use October\Rain\Database\Updates\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('app_projects_projects', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('app_projects_projects');
+    }
+}
